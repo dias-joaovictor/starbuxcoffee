86afdeb40dc3c19fa2b0a3dbe76d6bc3f6f851c3
João Dias <joao.dias@temasistemas.com.br>
2022-03-05 11:48:02 -0300

    First commit

---
+++ src/main/java/com/bestseller/starbuxcoffee/StarbuxcoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.bestseller.starbuxcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbuxcoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbuxcoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bestseller/starbuxcoffee/StarbuxcoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestseller.starbuxcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbuxcoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
